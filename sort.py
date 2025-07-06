4f8709797dbe080ce755a6a5c5111abe611a66aa
Pedro Jesus <pa04052007@gmail.com>
2025-07-05 23:25:06 -0300

    sort.py

--- sort.py
+++ sort.py
@@ -1,20 +1,18 @@
 from random import randint
-comp = randint(1,10)
-print(comp)
-tentativas = 1
-num = int(input('Pensei em um número de 1 a 10, tente adivinhar qual é:'))
-while comp != num:
-    if num > 10:
+random_number = randint(1,10)
+attemps = 1
+number = int(input('Pensei em um número de 1 a 10, tente adivinhar qual é:'))
+while random_number != number:
+    if number > 10:
         print('Número inválido')
-    if num > comp:
-        print('Quase lá, o número pensado é menor que {}'.format(num))
-    if num < comp:
-        print('Quase lá, o número pensado é maior que {}'.format(num))
+    if number > random_number:
+        print(f'Quase lá, o número pensado é menor que {number}')
+    if number < random_number:
+        print(f'Quase lá, o número pensado é maior que {number}')
     print('Mais sorte na próxima...')
-    tentativas += 1
-    num = int(input('Tente novamente:'))
-if tentativas == 1:
-    print('Você acertou de primeira!! em apenas {} tentativa, você chegou ao resultado, que era o número {}.'.format(tentativas, comp))
+    attemps += 1
+    number = int(input('Tente novamente:'))
+if attemps == 1:
+    print(f'Você acertou de primeira!! em apenas {attemps} tentativa, você chegou ao resultado, que era o número {random_number}.')
 else:
-    print('Parabéns!! o número certo era {}, Você chegou ao resultado em {} tentativas!!'.format(comp, tentativas))
- 
+    print(f'Parabéns!! o número certo era {random_number}, Você chegou ao resultado em {attemps} tentativas!!')
