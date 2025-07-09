aba9119179ace8962401991bbaa78516c772bff6
Pedro Jesus <pa04052007@gmail.com>
2025-07-09 01:29:13 -0300

    sort.py

--- sort.py
+++ sort.py
@@ -1,18 +1,29 @@
 from random import randint
-random_number = randint(1,10)
-attemps = 1
-number = int(input('Pensei em um número de 1 a 10, tente adivinhar qual é:'))
-while random_number != number:
-    if number > 10:
-        print('Número inválido')
-    if number > random_number:
-        print(f'Quase lá, o número pensado é menor que {number}')
-    if number < random_number:
-        print(f'Quase lá, o número pensado é maior que {number}')
-    print('Mais sorte na próxima...')
+print('='*30)
+print('''Bem vindo ao jogo do sorteio!!
+Tente adivinhar o número de 1 a 10 que o computador pensou.
+Ele te dará algumas dicas...
+No mais... Boa sorte!''')
+print('='*30)
+your_choice = int(input('Digite um número de 1 a 10:'))
+print('='*30)
+if your_choice > 10:
+    your_choice = int(input('Número inválido, tente novamente'))
+    print('='*30)
+computer_choice = randint(1,10)
+attemps = 0
+
+while your_choice != computer_choice:
     attemps += 1
-    number = int(input('Tente novamente:'))
-if attemps == 1:
-    print(f'Você acertou de primeira!! em apenas {attemps} tentativa, você chegou ao resultado, que era o número {random_number}.')
-else:
-    print(f'Parabéns!! o número certo era {random_number}, Você chegou ao resultado em {attemps} tentativas!!')
+    print('Você errou! mais sorte na próxima...')
+    if computer_choice > your_choice:
+        print(f'O número pensado é maior que {your_choice}')
+    elif computer_choice < your_choice:
+        print(f'O número pensado é menor que {your_choice}')
+    else:
+        print('Número inválido')
+    print('='*30)
+    your_choice = int(input('Tente novamente:'))
+    print('='*30)
+print(f'''Parabéns,você acertou!!! o número escolhido pelo computador era {computer_choice}.
+Você acertou em {attemps} tentativas.''')
